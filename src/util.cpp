3ec8517c3cafcd98cc7a36f41a1002012e970ab6
Jeija <jeija@mesecons.net>
2014-07-28 17:34:49 +0200

    Remove chunks of dead code

--- src/util.cpp
+++ src/util.cpp
@@ -8,25 +8,6 @@
 
 #include "util.h"
 
-/*
-	Aus BCU SDK Version 0.0.5, /bcusdk-0.0.5/eibd/examples/common.c
-*/
-
-eibaddr_t getGroupAddr (std::string addr)
-{
-	const char *addr_c = addr.c_str();
-	int a, b, c;
-
-	if (sscanf (addr_c, "%d/%d/%d", &a, &b, &c) == 3)
-		return ((a & 0x01f) << 11) | ((b & 0x07) << 8) | ((c & 0xff));
-	if (sscanf (addr_c, "%d/%d", &a, &b) == 2)
-		return ((a & 0x01f) << 11) | ((b & 0x7FF));
-	if (sscanf (addr_c, "%x", &a) == 1)
-		return a & 0xffff;
-
-	return 0x00;
-}
-
 void errorMessage(std::string msg)
 {
 	std::cerr << "Fehler: " << msg << "! Programm wird beendet." << std::endl;
--- src/util.h
+++ src/util.h
@@ -15,15 +15,6 @@ typedef uint16_t dmxaddr_t;
 typedef uint8_t dmxvalue_t;
 
 /*
-	Liest aus Gruppenadressen als const char in den Formaten
-	* "<Main group>/<Middle group>/<Subgroup>"
-	* "<Main Group>/<Subgroup>"
-	* "<Group>
-	die eibaddr_t - EIB-Adresse aus.
-*/
-eibaddr_t getGroupAddr (std::string addr);
-
-/*
 	Gibt die Nachricht (msg) aus und beendet das Programm mit Fehlerstatus.
 */
 void errorMessage(std::string msg);
@@ -32,14 +23,6 @@ void errorMessage(std::string msg);
 	Gibt die Nachricht (msg) aus, das Programm läuft aber weiter
 */
 void warningMessage(std::string msg);
-
-/*
-	Adresskonfigurationsdatei parsen
-	std::map<dmxaddr_t, std::string>, wobei
-	dmxaddr_t = dmx Adresse und
-	std::string = EIB Group Adresse
-*/
-std::map<dmxaddr_t, std::string> parseConfigFile(std::string filename);
 
 /*
 	Ordner, in dem die C++ - Datei ausgeführt wird erhalten
